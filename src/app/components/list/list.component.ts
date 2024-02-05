b41102790dd9d5aca193752d52272db58093f861
Csabán Balázs <Ba.Csaban@eos-ts.com>
2024-02-05 09:00:08 +0100

    lazyloading 1.1

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,14 +1,12 @@
-import { Component, Input, OnInit, effect, importProvidersFrom, signal } from '@angular/core';
-import { CharacterListService } from '../../services/characterList.service';
-import { character } from '../../types/rickAndMorty';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { ActivatedRoute, RouterModule, RouterOutlet } from '@angular/router';
+import { Component, ElementRef, Input, NgZone, OnDestroy, OnInit, effect, signal } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { Subject, of } from 'rxjs';
+import { delay, filter, switchMap, tap } from 'rxjs/operators';
+import { CharacterListService } from '../../services/characterList.service';
+import { character } from '../../types/rickAndMorty';
 import { ListItemComponent } from '../listItem/listItem.component';
-import { Location } from '@angular/common';
-import { routes } from '../../app.routes';
-import { debounceTime, mergeMap, switchMap, tap } from 'rxjs/operators';
-import { of } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -25,15 +23,20 @@ import { of } from 'rxjs';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent implements OnInit {
-  @Input('listPart') listPart:number = 0
+export class ListComponent implements OnInit, OnDestroy {
+  @Input('listPart') listPart: number = 0
   readonly page = signal(1);
+  readonly visibleSignal = signal(true);
+  readonly downloadDoneSignal = signal(false);
+  observer!: IntersectionObserver;
+  visibile = new Subject<Boolean>()
 
   listElements: character[] = []
 
   addEffect() {
     console.log('addEffect')
     effect((onCleanup) => {
+      console.log('visibleSignal:', this.visibleSignal())
       this.fetchData()
       onCleanup(() => {
         console.log("Perform cleanup action here");
@@ -41,31 +44,61 @@ export class ListComponent implements OnInit {
     });
   }
 
-  constructor(private route: ActivatedRoute, protected characterService: CharacterListService, private location: Location) {
+  constructor(
+    private el: ElementRef<HTMLElement>,
+    private route: ActivatedRoute,
+    protected characterService: CharacterListService,
+    private ngZone: NgZone) {
 
     this.addEffect()
+  }
+  ngOnDestroy(): void {
+
   }
 
   setPage(direction: number) {
     this.page.set(this.page() + direction)
   }
 
+  checkVisibility() {
+    this.ngZone.runOutsideAngular(() => {
+      this.observer = new IntersectionObserver((entries) => {
+        entries.forEach((e) => {
+          this.visibleSignal.set(e.isIntersecting)
+          if (!e.isIntersecting)
+            this.visibile.next(false);
+        });
+      });
+      this.observer.observe(this.el.nativeElement);
+    });
+  }
+
   ngOnInit() {
     const pageNumber = this.listPart.toString() ?? this.route.snapshot.paramMap.get('page')
 
     pageNumber && this.page.set(parseInt(pageNumber))
+
+    this.checkVisibility()
   }
 
   fetchData() {
-    of(this.page()).pipe(
-      tap((it)=>{
-        console.log('fetchData with page:' +it)
+    of(this.downloadDoneSignal()).pipe(
+      filter(it => !it),
+      tap((it) => {
+        console.log('fetchData with page:' + it)
       }),
-      debounceTime(1000),
-      mergeMap(it=>this.characterService.getCharacterList(it))
+      delay(100),
+      filter(() => !!this.visibleSignal()),
+      switchMap(it => this.characterService.getCharacterList(this.page())),
+      tap(() => {
+        console.log('FETCH END')
+        this.downloadDoneSignal.set(true)
+      })
     ).subscribe((it) => {
-     // this.location.replaceState("/list/" + this.page())
+      // this.location.replaceState("/list/" + this.page())
       this.listElements = it
     })
   }
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { InMemoryScrollingFeature, InMemoryScrollingOptions, RouterModule, provideRouter, withInMemoryScrolling } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
+
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'disabled',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature = withInMemoryScrolling(scrollConfig);
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [provideRouter(routes, inMemoryScrollingFeature)],
+})
   .catch((err) => console.error(err));
